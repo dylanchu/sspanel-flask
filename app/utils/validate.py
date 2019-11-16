fef735f87e2c966855e653f5b6481f4df0c2d7b8
dylanchu <chdy.uuid@gmail.com>
2019-11-17 04:57:55 +0800

    feat:utils:装饰器 admin_required

--- app/utils/validate.py
+++ app/utils/validate.py
@@ -4,6 +4,22 @@
 # Created by dylanchu on 19-2-28
 
 
+from functools import wraps
+from flask import jsonify
+from flask_login import current_user
+
+from app import errors
+
+
+def admin_required(func):
+    @wraps(func)
+    def decorated_view(*args, **kwargs):
+        if current_user.level < 90:
+            return jsonify(errors.Unauthorized)
+        return func(*args, **kwargs)
+    return decorated_view
+
+
 def is_local_url(url: str):
     if url.startswith('/'):
         return True
